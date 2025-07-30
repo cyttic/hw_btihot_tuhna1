b482ff054aa9f4d11db6299f61a1333d7833dcec
cyttic <cyttic@gmail.com>
2025-07-30 14:53:39 +0300

    step 3 task5

--- src/service.cpp
+++ src/service.cpp
@@ -9,14 +9,14 @@ using namespace std;
 map<string, vector<int>> getCommonSubVec(const string &arr){
 	map<string, vector<int>> result;
 	for(int i = 0; i < arr.size()-4; ++i){
-		for(int j = i+1; j < arr.size()-2; ++j){
-			string tmp(arr.begin()+i, arr.begin()+i+3);
-			if ( result.count(tmp) != 0)//check if result contains alredy this substring
+		string tmp(arr.begin()+i, arr.begin()+i+3);
+		if ( result.count(tmp) != 0)//check if result contains alredy this substring
 				continue;
+		for(int j = i+1; j < arr.size()-2; ++j){
 			if (arr[i] == arr[j] && (arr[i+1] == arr[j+1]) && (arr[i+2] == arr[j+2]) ){
 				if( result.count(tmp) == 0)//we need to add substring to result if it was found in first time
-					result[ tmp ].push_back(i);
-				result[ tmp ].push_back(j);
+					result[ tmp ].push_back(i+1);
+				result[ tmp ].push_back(j+1);
 			}
 		}
 	}
